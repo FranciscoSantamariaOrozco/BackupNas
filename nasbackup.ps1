78a92b5211ee561a11ed2928c67f5d05d744039c
Francisco Orozco VMW-win10-LenovoThinkPad-L380 <francisco.s.orozco@outlook.com>
2023-02-13 17:18:51 +0000

    minor fixes on nasbackup.ps1

--- nasbackup.ps1
+++ nasbackup.ps1
@@ -1,22 +1,21 @@
-
 $limit = (Get-Date).AddDays(-60)
 $files = Get-ChildItem -Path "C:/" -Recurse | Where-Object { $_.LastWriteTime -lt $limit }
 
-# # Import the SharePointPnPPowerShellOnline module
-# Import-Module SharePointPnPPowerShellOnline
+# Import the SharePointPnPPowerShellOnline module
+Import-Module SharePointPnPPowerShellOnline
 
-# # Connect to SharePoint Online
-# Connect-PnPOnline -Url "https://<tenant>.sharepoint.com/sites/<sitename>" -Credentials (Get-Credential)
+# Connect to SharePoint Online
+Connect-PnPOnline -Url "https://<tenant>.sharepoint.com/sites/<sitename>" -Credentials (Get-Credential)
 
-# # Loop through the old files and move them to SharePoint Online
-# foreach ($file in $files)
-# {
-#     # Specify the target folder in SharePoint Online
-#     $targetFolder = "/sites/<sitename>/Shared Documents/Old Files"
+# Loop through the old files and move them to SharePoint Online
+foreach ($file in $files)
+{
+    # Specify the target folder in SharePoint Online
+    $targetFolder = "/sites/<sitename>/Shared Documents/Old Files"
 
-#     # Upload the file to SharePoint Online
-#     Add-PnPFile -Path $file.FullName -Folder $targetFolder
+    # Upload the file to SharePoint Online
+    Add-PnPFile -Path $file.FullName -Folder $targetFolder
 
-#     # Output status message
-#     Write-Host "Moved file $($file.FullName) to $targetFolder"
-# }
+    # Output status message
+    Write-Host "Moved file $($file.FullName) to $targetFolder"
+}
