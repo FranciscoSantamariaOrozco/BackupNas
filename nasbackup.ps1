733405a8c0af32658929f5fd0fdff36dbacbdd77
Francisco Orozco ActiveIT-ThinkPad <francisco.s.orozco@outlook.com>
2023-02-13 10:49:04 +0000

    First commit

---
+++ nasbackup.ps1
@@ -0,0 +1,24 @@
+# $limit = (Get-Date).AddDays(-60)
+# Get-ChildItem -Path "C:/" -Recurse | Where-Object { $_.LastWriteTime -lt $limit } | Select-Object FullName, LastWriteTime | Format-Table -AutoSize
+
+$limit = (Get-Date).AddDays(-60)
+$files = Get-ChildItem -Path "C:/" -Recurse | Where-Object { $_.LastWriteTime -lt $limit }
+
+# Import the SharePointPnPPowerShellOnline module
+Import-Module SharePointPnPPowerShellOnline
+
+# Connect to SharePoint Online
+Connect-PnPOnline -Url "https://<tenant>.sharepoint.com/sites/<sitename>" -Credentials (Get-Credential)
+
+# Loop through the old files and move them to SharePoint Online
+foreach ($file in $files)
+{
+    # Specify the target folder in SharePoint Online
+    $targetFolder = "/sites/<sitename>/Shared Documents/Old Files"
+
+    # Upload the file to SharePoint Online
+    Add-PnPFile -Path $file.FullName -Folder $targetFolder
+
+    # Output status message
+    Write-Host "Moved file $($file.FullName) to $targetFolder"
+}
