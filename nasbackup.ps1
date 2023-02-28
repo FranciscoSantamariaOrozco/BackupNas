c1f597f5b99cdd3d32d1efee55193c16cefbe4f9
Francisco Orozco ActiveIT-ThinkPad <francisco.s.orozco@outlook.com>
2023-02-28 17:39:14 +0000

    Script listo - Pdte testeo

--- nasbackup.ps1
+++ nasbackup.ps1
@@ -1,34 +1,45 @@
-# Auto-Backup SharepointOnline
+# Auto-Backup to Sharepoint Online
 # Autor: Francisco Orozco
 
-Import-Module SharePointPnPPowerShellOnline
-
 # Limite de idade do ficheiro
+
 $limit = (Get-Date).AddDays(-60)
 
 # Path em que o scan será realizado
 # Ex: $pasta = "C:/"
+
 $pasta = "C:/probasscriptsharepoint/"
 
 # Dados sharepoint
 #
 # $urlsharepoint: é necessário indicar o endereço do sharepoint com o seu nºTenant (entre aspas).
 # Ex: $urlsharepoint = "https://<tenant>.sharepoint.com/sites/<sitename>"
+
 $urlsharepoint = "https://<tenant>.sharepoint.com/sites/<sitename>"
 
 # $urlworkdir: Pasta Sharepoint na qual as cópias serão guardadas.
 # Ex: $urlworkdir = "/sites/<sitename>/Shared Documents/Old Files"
+
 $urlworkdir = "/sites/<sitename>/Shared Documents/Old Files"
 
 # $username e $password: utilizador e palavra-passe autorizados a escrever para sharepoint.
 # Ex: $username = "user@domain.com"
 #     $password = ConvertTo-SecureString "yourpaswordhere" -AsPlainText -Force
+
 $username = "user@domain.com"
 $password = ConvertTo-SecureString "yourpaswordhere" -AsPlainText -Force
-$creds = New-Object System.Management.Automation.PSCredential ($username, $password)
 
+##############################################################################################################################################
+##############################################################################################################################################
 # Não modificar nada a partir desta linha.
 ##############################################################################################################################################
+##############################################################################################################################################
+# Importação do módulo sharepoint
+Import-Module SharePointPnPPowerShellOnline
+
+# Geração de credenciais.
+$creds = New-Object System.Management.Automation.PSCredential ($username, $password)
+
 # ligação sharepoint
 Connect-PnPOnline -Url $urlsharepoint -Credentials $creds
 
