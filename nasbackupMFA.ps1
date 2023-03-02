05637a164ac4a82055aff3575ab268d4ade32bbf
Francisco Orozco ActiveIT-ThinkPad <francisco.s.orozco@outlook.com>
2023-03-02 13:13:20 +0000

    Act script for MFA auth

---
+++ nasbackupMFA.ps1
@@ -0,0 +1,47 @@
+# Auto-Backup to Sharepoint Online with MFA
+# Autor: Francisco Orozco
+
+# Limite de idade do ficheiro
+
+$limit = (Get-Date).AddDays(-60)
+
+# Path em que o scan será realizado
+# Ex: $pasta = "C:/"
+
+$pasta = "C:/probasscriptsharepoint/"
+
+# Dados sharepoint
+# $urlsharepoint: é necessário indicar o endereço do sharepoint com o seu nºTenant (entre aspas).
+#
+# To find your Microsoft 365 tenant ID in the Azure AD admin center
+# Your tenant ID can be found in the Tenant ID box on the Properties page.
+# Ex: $urlsharepoint = "https://<tenant>.sharepoint.com/sites/<sitename>;"
+
+$urlsharepoint = "https://<tenant>.sharepoint.com/sites/<sitename>;"
+
+# $urlworkdir: Pasta Sharepoint na qual as cópias serão guardadas.
+# Ex: $urlworkdir = "/sites/<sitename>/Shared Documents/Old Files"
+
+$urlworkdir = "/sites/<sitename>/Shared Documents/Old Files"
+
+####################################################################################
+####################################################################################
+# Não modificar nada a partir desta linha.
+####################################################################################
+####################################################################################
+# Importação do módulo sharepoint
+Import-Module SharePointPnPPowerShellOnline
+
+# ligação sharepoint
+Connect-PnPOnline -Url $urlsharepoint -Credentials (Get-Credential)
+
+# detecção de ficheiros com mais de 60 dias.
+$files = Get-ChildItem -Path $pasta -Recurse | Where-Object { $_.LastWriteTime -lt $limit }
+
+# Percorrer os ficheiros e movê-los para SharePoint
+foreach ($file in $files)
+{
+    Add-PnPFile -Path $file.FullName -Folder $urlworkdir
+    Write-Host "Moved file $($file.FullName) to $urlworkdir"
+}
+####################################################################################
